22134de90bea0e47f68bd0b6c8aaa26fac6e67f3
mikelarg <murtazkorpaz@gmail.com>
2026-04-18 05:27:37 +0300

    - fixed base url bug - fixed ui dist bug when building giga_agent in docker

--- deployments/local/docker-entrypoint.frontend-nginx.sh
+++ deployments/local/docker-entrypoint.frontend-nginx.sh
@@ -2,9 +2,19 @@
 set -eu
 
 INDEX_HTML="/usr/share/nginx/html/index.html"
+INDEX_HTML_GZ="${INDEX_HTML}.gz"
 BASE_URL="${GIGA_AGENT_BASE_URL:-/}"
 
 if [ -f "$INDEX_HTML" ]; then
     escaped_base_url=$(printf '%s' "$BASE_URL" | sed 's/[\/&]/\\&/g')
-    sed -i "s|<base href=\"/\"/>|<base href=\"${escaped_base_url}\"/>|" "$INDEX_HTML"
+    sed -i \
+        -e "s|<base href=\"/\"/>|<base href=\"${escaped_base_url}\"/>|" \
+        -e "s|<base href=\"./\"/>|<base href=\"${escaped_base_url}\"/>|" \
+        -e "s|<base href=\"/\">|<base href=\"${escaped_base_url}\"/>|" \
+        -e "s|<base href=\"./\">|<base href=\"${escaped_base_url}\"/>|" \
+        "$INDEX_HTML"
+
+    if [ -f "$INDEX_HTML_GZ" ]; then
+        gzip -c -9 "$INDEX_HTML" > "$INDEX_HTML_GZ"
+    fi
 fi
